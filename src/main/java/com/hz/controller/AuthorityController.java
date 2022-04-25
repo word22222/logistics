cdd4e00791aada2ec261988f683ab52451923aea
word <994407279@qq.com>
2022-04-25 11:26:28 +0800

    修改

---
+++ src/main/java/com/hz/controller/AuthorityController.java
@@ -0,0 +1,23 @@
+package com.hz.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author UserG
+ * @since 2022-04-25
+ */
+@RestController
+@RequestMapping("/authority")
+public class AuthorityController {
+public void aas(){
+
+}
+}
+
