6c83fb8a3625e77ef0bc065f581f83e2c37512af
word <994407279@qq.com>
2022-04-25 11:28:52 +0800

    修改了

---
+++ src/main/java/com/hz/controller/AuthorityRoleController.java
@@ -0,0 +1,23 @@
+package com.hz.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author UserG
+ * @since 2022-04-25
+ */
+@RestController
+@RequestMapping("/authority-role")
+public class AuthorityRoleController {
+public void bb(){
+
+}
+}
+
