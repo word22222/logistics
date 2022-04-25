68cd6ae1fa649b912dec338ea3250269567911cc
word <994407279@qq.com>
2022-04-25 11:42:57 +0800

    修改

--- src/main/java/com/hz/controller/AuthorityRoleController.java
+++ src/main/java/com/hz/controller/AuthorityRoleController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/authority-role")
 public class AuthorityRoleController {
 public void bb(){
-
+    System.out.println(123);
 }
 }
 
