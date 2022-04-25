b0aaa9cc4d8c37f1df8c6181ff630cf789575267
word <994407279@qq.com>
2022-04-25 11:46:25 +0800

    输出了

--- src/main/java/com/hz/controller/AuthorityController.java
+++ src/main/java/com/hz/controller/AuthorityController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/authority")
 public class AuthorityController {
 public void aas(){
-
+    System.out.println(11111);
 }
 }
 
